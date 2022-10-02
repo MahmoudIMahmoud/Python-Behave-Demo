0ebb5d7053ca4af3b955b022033a8725c0aca11b
mahmoudimahmoud <mahmoud.ibrahim.1396@gmail.com>
2022-10-02 15:51:02 +0200

    added a parameter

--- features/sample.feature
+++ features/sample.feature
@@ -2,5 +2,5 @@ Feature: Sample1
 
 Scenario: Scenario 1
 Given a setup for the scenario
-When  some action is done
+When  some action with parameter "my param" is done
 Then  I should found something
--- features/steps/step.impl.py
+++ features/steps/step.impl.py
@@ -4,9 +4,11 @@ def step_impl(context):
     print(u'STEP: Given a setup for the scenario')
 
 
-@when(u'some action is done')
-def step_impl(context):
+@when(u'some action with parameter {param} is done')
+def step_impl(context,param):
     print(u'STEP: When some action is done')
+    print(f"The parameter passed is {param}")
+    
 
 
 @then(u'I should found something')
